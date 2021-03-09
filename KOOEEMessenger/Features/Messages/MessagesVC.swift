198ad9e8c4844aa3995f316d308389fbc0e89ffe
Vijay <sharmav9753@gmail.com>
2021-03-09 12:38:27 +0530

    Add Functionality to Message Class

--- KOOEEMessenger/Features/Messages/MessagesVC.swift
+++ KOOEEMessenger/Features/Messages/MessagesVC.swift
@@ -2,5 +2,19 @@ import Foundation
 import UIKit
 
 class MessagesVC: UIViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let image = UIImage(systemName: "line.horizontal.3")
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: image,
+            style: .plain, target: self, action: #selector(showSettings)
+        )
+    }
+
+    @objc func showSettings() {
+        let settingsStoryboard = UIStoryboard(name: "Settings", bundle: nil)
+        self.navigationController?.present(
+            settingsStoryboard.instantiateViewController(withIdentifier: "Settings"), animated: true
+        )
+    }
 }
---
+++ KOOEEMessenger/Reusable Classes/RoundedCornerView.swift
@@ -0,0 +1,8 @@
+//
+//  RoundedCornerView.swift
+//  KOOEEMessenger
+//
+//  Created by Deqode on 02/03/21.
+//
+
+import Foundation
